a6ba41bdae194d230514713085d7e7d8135248b3
clydeev <clydevelasquez@outlook.com>
2016-11-19 01:19:07 +0800

    Solve processEmail.m

--- Week 7/machine-learning-ex6/ex6/processEmail.m
+++ Week 7/machine-learning-ex6/ex6/processEmail.m
@@ -96,17 +96,14 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
-
-
-
-
-
-
-
-
-
-
-    % =============================================================
+	index = 1;
+	for j = 1:length(vocabList);
+		if (strcmp(vocabList{j}, str))
+			word_indices = [word_indices; j];
+		endif
+	endfor
+	
+	% =============================================================
 
 
     % Print to screen, ensuring that the output lines are not too long
