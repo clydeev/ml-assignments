5fc2e8f301207cc8b996443429151c713c9a464a
clydeev <clydevelasquez@outlook.com>
2016-12-04 23:00:02 +0800

    Solve cost function and gradient computation

--- Week 9/machine-learning-ex8/ex8/cofiCostFunc.m
+++ Week 9/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -39,21 +39,19 @@ Theta_grad = zeros(size(Theta));
 %        Theta_grad - num_users x num_features matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
 %
+	
+	% Element wise multiplication of R to exclude users who have not rated a movie
+	J = (1 / 2) * sum(sum((((X * Theta') - Y) .* R) .^ 2));
 
+	% Compute gradients of X and Theta
+	X_grad = (((X * Theta') - Y) .* R) * Theta;
+	Theta_grad = (((X * Theta') - Y) .* R)' * X;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	if (lambda > 0) 
+		J = J + ((lambda / 2) * sum(sum(Theta .^ 2))) + ((lambda / 2) * sum(sum(X .^ 2)));
+		X_grad = X_grad + (lambda * X);
+		Theta_grad = Theta_grad + (lambda * Theta);
+	end
 
 % =============================================================
 
